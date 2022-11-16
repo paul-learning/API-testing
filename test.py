1700242453d314bb8c46c23dcb4efca297aa0144
Paul <paulhinterberger@gmail.com>
2022-11-16 10:24:32 +0100

    Edit on html to api connection

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import requests
 
-URL = "https://api.coinbase.com/v2/prices/BTC-EUR/spot"
 
-response = requests.get(URL)
+def get_bitcoin():
+    URL = "https://api.coinbase.com/v2/prices/BTC-EUR/spot"
+    response = requests.get(URL)
+    return(response.text)
 
-print(response.text)
+print(get_bitcoin())
